caec0b12bf927d8313dff311f3986355096ed4d6
Tom Britton <britton.tm@gmail.com>
2023-01-14 13:53:06 -0600

    Karaoke scraper first commit.

---
+++ index.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { load } from "cheerio";
+import { KaraokeSong } from "./types";
+
+const url = "https://www.karaokenerds.com/GlobalKaraokeCommunity/Browse";
+
+enum DataMap {
+  "artist" = 0,
+  "title" = 1,
+  "date" = 2,
+  "uploader" = 3,
+  "youtubeId" = 4,
+}
+
+const scrapeKaraokeData = (): Promise<KaraokeSong[]> => {
+  return axios.get(url).then(({ data }) => {
+    const $ = load(data);
+    const rows = $(".table tr");
+    const songList: KaraokeSong[] = [];
+    rows.map((_, row) => {
+      const tableCells = $(row).find("td");
+      const song = {
+        artist: $(tableCells[DataMap["artist"]])?.text()?.trim(),
+        title: $(tableCells[DataMap["title"]])?.text()?.trim(),
+        date: $(tableCells[DataMap["date"]])?.text()?.trim(),
+        uploader: $(tableCells[DataMap["uploader"]])?.text()?.trim(),
+        youtubeId: $(tableCells[DataMap["youtubeId"]])
+          ?.find("a")
+          ?.attr("href")
+          ?.split("=")[1]
+          ?.trim(),
+      };
+      if (song.artist && song.title && song.youtubeId) {
+        songList.push(song);
+      }
+    });
+    return songList;
+  });
+};
+
+export { scrapeKaraokeData };
---
+++ types.ts
@@ -0,0 +1,7 @@
+export type KaraokeSong = {
+  artist: string;
+  title: string;
+  date: string;
+  uploader: string;
+  youtubeId: string;
+};
